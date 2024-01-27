8322ad610dc822eef559518860300c22e9ae8f54
MitchZas <mitchellzasa@gmail.com>
2024-01-27 15:11:34 -0500

    First pass at animations
    Hooked it up so that the player changes state when walking vs idling

--- Sketch Quest/Assets/Scripts/PlayerMovement.cs
+++ Sketch Quest/Assets/Scripts/PlayerMovement.cs
@@ -8,16 +8,31 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public PlayerInput pInput;
     public float speed = 6f;
-    // Update is called once per frame
 
+    private Animator animator;
+
+    void Start() 
+    {
+       animator = GetComponent<Animator>(); 
+    }
+    
     void Update()
     {
         Vector2 movement = pInput.actions["Move"].ReadValue<Vector2>();
         Vector3 direction = new Vector3(movement.x, 0f, movement.y).normalized;
 
-        if (direction.magnitude >= 0.1f)
+        if (movement.magnitude >= 0.1f)
         {
             controller.Move(direction * speed * Time.deltaTime);
+        }
+
+        if(movement == Vector2.zero)
+        {
+            animator.SetFloat("Speed",0);
+        }
+        else
+        {
+            animator.SetFloat("Speed", 1f);
         }
     }
 }
