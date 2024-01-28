d5f2f1db8dfc9bcea84970a833737063c6bb177f
Nexmurco <perkinsnathan97@gmail.com>
2024-01-28 13:58:08 -0500

    SoundController Update

--- Sketch Quest/Assets/SoundController.cs
+++ Sketch Quest/Assets/SoundController.cs
@@ -4,13 +4,28 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    public MicController micControllerScript;
+    private MicController micControllerScript;
+    private SoundController sCtrl;
+
+    public AudioClip sneak;
+    public AudioClip shine;
+    public AudioClip ouch;
+    public AudioClip water;
+    public AudioClip danger;
+    public AudioClip quip;
     
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         micControllerScript = GameObject.Find("MicController").GetComponent<MicController>();
+        SoundController = GameObject.Find("SoundController").GetComponent<SoundController>();
         Debug.Log("YO HEAR ME");
+        sneak = micControllerScript.clip1;
+        shine = micControllerScript.clip2;
+        ouch = micControllerScript.clip3;
+        water = micControllerScript.clip4;
+        danger = micControllerScript.clip5;
+        quip = micControllerScript.clip6;
     }
 
     // Update is called once per frame
