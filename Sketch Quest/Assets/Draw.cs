ea5d580fbb38950b094307460d5f118f496000e9
JonTabiendo <jontabiendo@gmail.com>
2024-01-27 20:42:01 -0500

    'drawing_progressing'

--- Sketch Quest/Assets/Draw.cs
+++ Sketch Quest/Assets/Draw.cs
@@ -23,6 +23,7 @@ public class Draw : MonoBehaviour
         else if (Input.GetKey(KeyCode.Mouse0))
         {
             PointToMousePos();
+            Debug.Log("continuing line...");
         }
         else 
         {
@@ -37,25 +38,35 @@ public class Draw : MonoBehaviour
 
 
         //because you gotta have 2 points to start a line renderer, 
-        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Input.mousePosition;
 
+        Debug.Log(mousePos);
         currentLineRenderer.SetPosition(0, mousePos);
+        // Debug.Log("CHECK 1");
+        // lastPos = mousePos;
+        currentLineRenderer.positionCount++;
         currentLineRenderer.SetPosition(1, mousePos);
-
+        // Debug.Log("CHECK 2");
     }
 
     void AddAPoint(Vector2 pointPos) 
     {
+        Debug.Log(currentLineRenderer.positionCount);
         currentLineRenderer.positionCount++;
+        // currentLineRenderer.size++;
+        // Debug.Log(currentLineRenderer.positionCount);
         int positionIndex = currentLineRenderer.positionCount - 1;
+        Debug.Log(currentLineRenderer.positionCount);
         currentLineRenderer.SetPosition(positionIndex, pointPos);
+        Debug.Log("CHECK");
     }
 
     void PointToMousePos() 
     {
-        Vector2 mousePos = m_camera.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Input.mousePosition;
         if (lastPos != mousePos) 
         {
+            Debug.Log("ADDING POSITION");
             AddAPoint(mousePos);
             lastPos = mousePos;
         }
